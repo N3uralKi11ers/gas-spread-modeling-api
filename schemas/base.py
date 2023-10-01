9032999eb1e904eb86e1d24d7860ddefc9d084c4
Nikolay Simakov <n.simakoff@gmail.com>
2023-10-01 11:24:33 +0300

    Update models basic methods

--- schemas/base.py
+++ schemas/base.py
@@ -2,13 +2,20 @@ import json
 from PIL import Image
 from io import BytesIO
 from pydantic import BaseModel, validator
-from typing import List, Optional
+from typing import List, Optional, Tuple
 from enum import Enum
 from fastapi import FastAPI, Form, UploadFile, File
 
 class Pos(BaseModel):
     x: int
     y: int
+    
+    def to_tuple(self) -> Tuple[int, int]:
+        return (self.x, self.y)
+    
+    def to_tuple_yx(self) -> Tuple[int, int]:
+        return (self.y, self.x)
+    
 
 class GasType(Enum):
     test_gas = 0
@@ -35,13 +42,18 @@ class BaseElement(Enum):
     gas = 3
 
 class EvacuationMap(BaseModel):
+    
     ev_map: List[List[BaseElement]]
+    
     def pprint(self):
         for i in range(len(self.ev_map)):
             row = ""
             for j in range(len(self.ev_map[i])):
                 row += f"{self.ev_map[i][j].value} "
             print(row)
+    
+    def to_array(self) -> List[List[int]]:
+        return [[v.value for v in _map] for _map in self.ev_map]
 
 class EvacuationMapTimeSeries(BaseModel):
     maps_series: List[EvacuationMap]
