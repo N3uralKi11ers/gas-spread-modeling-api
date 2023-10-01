9563860c82c402e691c9b0c8fc7596ef16017186
Ilya Lyutoev <lyutoev2003ia@gmail.com>
2023-10-01 10:44:12 +0300

    add docker

--- api/routes.py
+++ api/routes.py
@@ -6,9 +6,7 @@ from copy import deepcopy
 
 router = APIRouter(prefix="/api/v1")
 
-@router.post("/process_"
-            #  , response_model=EvacuationMapTimeSeries
-             )
+@router.post("/process_time_series", response_model=EvacuationMapTimeSeries)
 async def start_solve(data: BaseSettings):
     ev_map = data.evacuation_map
     for gas in data.gases:
@@ -28,6 +26,6 @@ async def start_solve(data: BaseSettings):
 async def post_photo(file: UploadFile = File(...)):
     content = await file.read()
     ev_map = process_photo_in_evacuation_map(content)
-    ev_map.pprint()
+    # ev_map.pprint()
     return ev_map
 
